a334e8ec1222e84ba8e7edc3b4e8da467af139e3
Jhosias007 <jhosias007@gmail.com>
2023-04-19 15:05:00 -0500

    Add index.html, main.js and styles.css

---
+++ README.md
@@ -0,0 +1,1 @@
+# Principal Page from Jhosias
---
+++ main.js
@@ -0,0 +1,9 @@
+const pMesage = document.querySelector("#pMessage");
+const btnChangeColor = document.querySelector("#btnChangeColor");
+
+btnChangeColor.addEventListener("click", fnChangeColor);
+function fnChangeColor () {
+  pMesage.classList.toggle("red",);
+  pMesage.classList.toggle("blue");
+
+}
