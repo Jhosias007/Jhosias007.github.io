446fb43177d1912c798438f9213c1c95218b5a28
Jhosias007 <jhosias007@gmail.com>
2023-04-28 20:17:35 -0500

    Antes de prueba.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const btnChangeColor = document.querySelector("#btnChangeColor");
 
 btnChangeColor.addEventListener("click", fnChangeColor);
 function fnChangeColor () {
-  pMesage.classList.toggle("red",);
+  pMesage.classList.toggle("red");
   pMesage.classList.toggle("blue");
 
 }
